fe4561251b21bf649f679baeeabfed0ea2e5eea3
sofiabarcelonna-bi <sofibarcelonna@gmail.com>
2025-10-07 20:16:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-// ---
-const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
-const smallMenu = document.querySelector('.header__sm-menu')
-const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
-const headerHamMenuCloseBtn = document.querySelector(
-  '.header__main-ham-menu-close'
-)
-const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+// --- MENÚ HAMBURGUESA ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont');
+const smallMenu = document.querySelector('.header__sm-menu');
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu');
+const headerHamMenuCloseBtn = document.querySelector('.header__main-ham-menu-close');
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link');
 
 hamMenuBtn.addEventListener('click', () => {
-  if (smallMenu.classList.contains('header__sm-menu--active')) {
-    smallMenu.classList.remove('header__sm-menu--active')
-  } else {
-    smallMenu.classList.add('header__sm-menu--active')
-  }
-  if (headerHamMenuBtn.classList.contains('d-none')) {
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
-  } else {
-    headerHamMenuBtn.classList.add('d-none')
-    headerHamMenuCloseBtn.classList.remove('d-none')
-  }
-})
+  smallMenu.classList.toggle('header__sm-menu--active');
 
-for (let i = 0; i < headerSmallMenuLinks.length; i++) {
-  headerSmallMenuLinks[i].addEventListener('click', () => {
-    smallMenu.classList.remove('header__sm-menu--active')
-    headerHamMenuBtn.classList.remove('d-none')
-    headerHamMenuCloseBtn.classList.add('d-none')
-  })
-}
+  headerHamMenuBtn.classList.toggle('d-none');
+  headerHamMenuCloseBtn.classList.toggle('d-none');
+});
 
-// ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
+headerSmallMenuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active');
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  });
+});
 
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
-})
+// --- LOGO REDIRECCIONA AL INICIO ---
+const headerLogoContainer = document.querySelector('.header__logo-container');
+
+headerLogoContainer.addEventListener('click', () => {
+  location.href = 'index.html';
+});
